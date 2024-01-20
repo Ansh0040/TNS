43bac84fa03a86901c1822eea7b8d515d2ea5319
Ansh Singh <singhansh040@gmail.com>
2024-01-20 10:24:52 +0530

    n

--- src/com/arya/helloworld/HelloWorld.java
+++ src/com/arya/helloworld/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.arya.helloworld;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+public void show() {
+    System.out.println("Hello World");
+}
 }
